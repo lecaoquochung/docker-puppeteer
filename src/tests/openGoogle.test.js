91046bafc1027f4a14568d503eebfe97ce40fcbd
lecaoquochung <lecaoquochung@gmail.com>
2020-07-12 15:35:07 +0900

    puppeteer no-sandbox

--- src/tests/openGoogle.test.js
+++ src/tests/openGoogle.test.js
@@ -9,14 +9,22 @@ describe(
     let browser ;
 
     beforeAll(async () => {
-      browser = await puppeteer.launch({});
+      // Running as root without --no-sandbox is not supported. See https://crbug.com/638180
+      // https://app.circleci.com/pipelines/github/lecaoquochung/docker-puppeteer/41/workflows/7b4e4ab8-b8b2-46c6-9d21-f87abcd4543b/jobs/42
+      // browser = await puppeteer.launch({});
+      // https://github.com/karma-runner/karma-chrome-launcher/issues/158
+      // Research sandbox
+      // https://unix.stackexchange.com/questions/68832/what-does-the-chromium-option-no-sandbox-mean
+      // https://www.google.com/googlebooks/chrome/big_00.html
+      browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
       page = await browser.newPage();
       await page.goto('https://google.co.jp');
     }, timeout)
 
-    afterAll(async () => {
+    afterAll(async done => {
       await page.close();
       await browser.close();
+      done();
     })
 
     it('should load without error', async () => {
