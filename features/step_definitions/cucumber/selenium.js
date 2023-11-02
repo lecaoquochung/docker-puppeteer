bc56b532ae7b88ae2e03b282b0f3230d751a299f
lehungio <me@lehungio.com>
2023-11-02 09:38:50 +0900

    Circleci - cache path & selenium headless config

--- features/step_definitions/cucumber/selenium.js
+++ features/step_definitions/cucumber/selenium.js
@@ -1,17 +1,11 @@
 const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 const { Builder, By, Capabilities, Key, until } = require('selenium-webdriver');
-
 const { expect } = require('chai');
-
 require("chromedriver");
-
-// Configure chrome headlease mode
-const chrome = require('selenium-webdriver/chrome');
-const chromeOptions = new chrome.Options().headless(); // TODO: switch to true for headless mode
 
 // driver setup
 const capabilities = Capabilities.chrome();
-capabilities.set('chromeOptions', { "w3c": false });
+capabilities.set('chromeOptions', { "w3c": false, args: ["--headless"] }); // Enable headless mode
 const driver = new Builder().withCapabilities(capabilities).build();
 
 Given('I am on the Google search page', { timeout: 30000 }, async function () {
@@ -22,7 +16,6 @@ When('I search for {string}', { timeout: 30000 }, async function (searchTerm) {
     const element = await driver.findElement(By.css('textarea'));
     await element.sendKeys(searchTerm);
     await element.submit();
-
 });
 
 Then('the page title should start with {string}', {timeout: 60 * 1000}, async function (searchTerm) {
